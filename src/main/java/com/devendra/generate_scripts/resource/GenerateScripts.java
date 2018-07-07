a374628fc685e9ec273108140e6b0cf486cec5b8
Devendra Tummala <kanna.phani.9@gmail.com>
2018-07-07 16:23:37 +0530

    Updated Options

--- src/main/java/com/devendra/generate_scripts/resource/GenerateScripts.java
+++ src/main/java/com/devendra/generate_scripts/resource/GenerateScripts.java
@@ -25,148 +25,85 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/generatescripts")
 public class GenerateScripts {
-	
+
 	@PostMapping
-	public String generateScript(@RequestParam("file")MultipartFile file,@RequestParam("tableName")String tableName,
-			@RequestParam("maxRows")int maxRows,
-			@RequestParam("columnNamesRowNum")int columnNamesRowNum,
-			@RequestParam("integerColumns")String integerColumns)
-					throws InvalidFormatException, IOException {
-		Workbook wb=getWorkbook(file);
-		
+	public String generateScript(@RequestParam("file") MultipartFile file, @RequestParam("tableName") String tableName,
+			@RequestParam("limitRows") String limitRows, @RequestParam("startingRowNum") String startingRowNum,
+			@RequestParam("integerColumns") String integerColumns) throws InvalidFormatException, IOException {
+		Workbook wb = getWorkbook(file);
 		Sheet sheet = wb.getSheetAt(0);
-	    Row row;
-	    Cell cell;
-	  
-	    StringBuilder query = new StringBuilder();
-	    query.append("insert into "+tableName+" (");
-	    
-	    int headerColumnsCount=0;
-	    
-	    DataFormatter formatter = new DataFormatter();
-	    List<String> integerList=Arrays.asList(integerColumns.split(","));
-	    System.out.println(integerList.size());
-	    row=sheet.getRow(columnNamesRowNum-1);
-	    headerColumnsCount=row.getPhysicalNumberOfCells();
-	    for(int i=0;i<headerColumnsCount;i++) {
-			   
-			   if(i==headerColumnsCount-1)
-				   query.append(row.getCell(i)+") values(");
-			   else
-				   query.append(row.getCell(i)+",");
-		   }
-		   for(int i=columnNamesRowNum;i<maxRows;i++) {
-			   row=sheet.getRow(i);
-			   int pCells=row.getPhysicalNumberOfCells();
-			   System.out.println(pCells);
-			   for(int j=0;j<headerColumnsCount;j++) {
-				   cell=row.getCell(j);
-				   String val = formatter.formatCellValue(cell);
-				   if(val.isEmpty()) {
-					   val=null;
-					   query.append(val);
-				   }
-				   else if(integerList.contains(Integer.toString(j+1))) {
+		Row row;
+		Cell cell;
+		int startRow = 1;
+		int limitToRows = sheet.getLastRowNum();
+		int noOfColumns = 0;
+		StringBuilder query = new StringBuilder();
+		query.append("insert into " + tableName + " (");
+		if (!limitRows.equals("")) {
+			limitToRows = Integer.valueOf(limitRows);
+		}
+		if (!startingRowNum.equals("")) {
+			startRow = Integer.valueOf(startingRowNum);
+		}
+
+		DataFormatter formatter = new DataFormatter();
+		List<String> integerList = Arrays.asList(integerColumns.split(","));
+		row = sheet.getRow(startRow - 1);
+		noOfColumns = row.getLastCellNum();
+		for (int i = 0; i < noOfColumns; i++) {
+
+			if (i == noOfColumns - 1)
+				query.append(row.getCell(i) + ") values(");
+			else
+				query.append(row.getCell(i) + ",");
+		}
+		for (int i = startRow; i <= limitToRows; i++) {
+			row = sheet.getRow(i);
+			for (int j = 0; j < noOfColumns; j++) {
+				cell = row.getCell(j);
+				String val = formatter.formatCellValue(cell);
+				if (val.isEmpty()) {
+					val = null;
 					query.append(val);
-				    }
-				    else {
-				    	query.append("'"+val+"'");
-				    }
-				    if(j==headerColumnsCount-1)
-				    	query.append(")");
-				    else
-				    	query.append(",");
-			   }
-			   if(i!=maxRows-1) {
-				   query.append(",(");
-			   }
-		   }
-		   
-		   System.out.println(query.toString());
-	
+				} else if (integerList.contains(Integer.toString(j + 1))) {
+					query.append(val);
+				} else {
+					query.append("'" + val + "'");
+				}
+				if (j == noOfColumns - 1)
+					query.append(")");
+				else
+					query.append(",");
+			}
+			if (i != limitToRows) {
+				query.append(",(");
+			}
+		}
 		return query.toString();
-		
+
 	}
+
 	public Workbook getWorkbook(MultipartFile file) throws InvalidFormatException, IOException {
 		Workbook wb = null;
 		byte[] bytes = file.getBytes();
-		String fileType=getFileExtension(file.getOriginalFilename());
-		File tempFile=File.createTempFile("abc", "xlsx");
-		FileOutputStream fos=new FileOutputStream(tempFile);
+		String fileType = getFileExtension(file.getOriginalFilename());
+		File tempFile = File.createTempFile("abc", "xlsx");
+		FileOutputStream fos = new FileOutputStream(tempFile);
 		fos.write(bytes);
 		fos.close();
-		System.out.println(file.getOriginalFilename());
-		if(fileType.equalsIgnoreCase("xlsx")) {
+		if (fileType.equalsIgnoreCase("xlsx")) {
 			wb = new XSSFWorkbook(tempFile);
-		}
-		else if(fileType.equalsIgnoreCase("xls")) {
-			wb=new HSSFWorkbook(new POIFSFileSystem(new FileInputStream(tempFile)));
+		} else if (fileType.equalsIgnoreCase("xls")) {
+			wb = new HSSFWorkbook(new POIFSFileSystem(new FileInputStream(tempFile)));
 		}
 		return wb;
-		
+
 	}
-	
-	public static void main(String... args) throws IOException, InvalidFormatException {
-		Workbook wb=null;
-		
-			//Workbook wb = new XSSFWorkbook(new File("E:\\7LP\\Scripts\\for insert.xlsx"));
-		 wb=new HSSFWorkbook(new POIFSFileSystem(new FileInputStream("E:\\7LP\\Scripts\\for insert.xlsx")));
-			Sheet sheet = wb.getSheetAt(0);
-		    Row row;
-		    Cell cell;
-		    String tableName="rfr_pgs_fspevent";
-		    StringBuilder query = new StringBuilder();
-		    query.append("insert into "+tableName+" (");
-		    int maxRows=3;
-		    int headerColumnsCount=0;
-		    int columnNamesRow=1;
-		    DataFormatter formatter = new DataFormatter();
-		    List<Integer> integerList=Arrays.asList();
-		    row=sheet.getRow(columnNamesRow-1);
-		    headerColumnsCount=row.getPhysicalNumberOfCells();
-		   for(int i=0;i<headerColumnsCount;i++) {
-			   
-			   if(i==headerColumnsCount-1)
-				   query.append(row.getCell(i)+") values(");
-			   else
-				   query.append(row.getCell(i)+",");
-		   }
-		   for(int i=columnNamesRow;i<maxRows;i++) {
-			   row=sheet.getRow(i);
-			   int pCells=row.getPhysicalNumberOfCells();
-			   System.out.println(pCells);
-			   for(int j=0;j<headerColumnsCount;j++) {
-				   cell=row.getCell(j);
-				   String val = formatter.formatCellValue(cell);
-				   if(val.isEmpty()) {
-					   val=null;
-					   query.append(val);
-				   }
-   				   else if(integerList.contains(j+1)) {
-   					query.append(val);
-				    }
-				    else {
-				    	query.append("'"+val+"'");
-				    }
-				    if(j==headerColumnsCount-1)
-				    	query.append(")");
-				    else
-				    	query.append(",");
-			   }
-			   if(i!=maxRows-1) {
-				   query.append(",(");
-			   }
-		   }
-		   
-		   System.out.println(query.toString());
-		}
-	
+
 	private static String getFileExtension(String fileName) {
-        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
-        return fileName.substring(fileName.lastIndexOf(".")+1);
-        else return "";
-    }
+		if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+			return fileName.substring(fileName.lastIndexOf(".") + 1);
+		else
+			return "";
 	}
-
-
-
+}
